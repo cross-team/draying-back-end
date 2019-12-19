363886180ebe976e01e56830ed4c8bd741538a7a
Sebastian <sebastian@softdev.team>
2019-12-18 23:17:47 -0500

    setting allow url

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ const server = new ApolloServer({
 })
 const app = express()
 const corsOptions = {
-  origin: process.env.FRONT_END_URL,
+  origin: process.env.FRONT_END_URL || 'https://draying.netlify.com/',
   credentials: true, // <-- REQUIRED backend setting
 }
 app.use('*', cookieParser())
